33056331d2f757d160e9f7a2e93a77719a2e7be2
Xi Matriarx <xi@ximatriarx.io>
2024-02-15 14:08:11 +0200

    description

--- bin/init.sh
+++ bin/init.sh
@@ -8,7 +8,7 @@ then
 	exit
 fi
 
-git clone https://github.com/XiMatriarx/typescript.git --depth 1 --branch 'v1.0.3'
+git clone https://github.com/XiMatriarx/typescript.git --depth 1 --branch 'v1.0.4'
 mv typescript $project
 cd $project
 rm -Rf .git
